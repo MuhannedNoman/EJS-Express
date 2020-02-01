4d4374f8a1bde1855b23a706b3151341770b63f2
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-02-01 21:35:45 +0300

    Sent the index.ejs to the client

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const path = require('path');
-
 const app = express();
+
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.set('view engine','ejs');
+
 app.get("/", (request, response) => {
-  response.sendfile(path.join(__dirname,'static','index.html'));
+  response.render('index');
 });
 
-app.get("/test", (request, response) => {
-  response.send("Your in test route");
-});
+// app.get("/test", (request, response) => {
+//   response.send("Your in test route");
+// });
 
-app.get("/test/:name/:age", (request, response) => {
-    console.log(request.query);
-  response.send(request.params.name + " : " + request.params.age)
-});
+// app.get("/test/:name/:age", (request, response) => {
+//     console.log(request.query);
+//   response.send(request.params.name + " : " + request.params.age)
+// });
 
 app.listen(3005);
